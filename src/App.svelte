0db9ec7caaa4467dc33d0556beaa8670abbca16b
Gonzalo Rivero Baud <riverobaudg@gmail.com>
2021-12-13 20:23:05 -0300

    #20 Reusable Tabs Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,26 @@
 <script>
 	import Header from "./componets/Header.svelte";
 	import Footer from "./componets/Footer.svelte";
+	import Tab from "./shared_components/Tabs.svelte";
+
+	//tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+
+	const tabChange = (e) => {
+		activeItem = e.detail;
+	}
 </script>
 
 
 <Header />
 <main>
-	
+	<Tab {items} {activeItem} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Polls'}
+		<p>Lista de memes</p>
+	{:else if activeItem === 'Add New Poll'}
+		<p>Agregar meme</p>
+	{/if}
 </main>
 <Footer />
 
